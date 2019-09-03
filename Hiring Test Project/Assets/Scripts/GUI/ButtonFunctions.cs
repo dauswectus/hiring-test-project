949e84621b45ea2e4f63abe50b29efb57c4e8696
dauswectus <dausw93@gmail.com>
2019-09-03 21:45:51 +0200

    Base UI added
    Main menu canvas with buttons added with dummy onclick functions
    Main playfield UI canvas added

---
+++ Hiring Test Project/Assets/Scripts/GUI/ButtonFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    void Start ()
+    {
+        switch (transform.name)
+        {
+            case "Start":
+                GetComponent<Button>().onClick.AddListener(StartGame);
+                break;
+            case "Options":
+                GetComponent<Button>().onClick.AddListener(SetOptions);
+                break;
+            case "Exit":
+                GetComponent<Button>().onClick.AddListener(ExitGame);
+                break;
+            default:
+                break;
+        }
+
+    }
+    public void StartGame()
+    {
+        Debug.Log("1");
+    }
+    void SetOptions()
+    {
+        Debug.Log("2");
+    }
+    void ExitGame()
+    {
+        Debug.Log("3");
+    }
+
+}
---
+++ Hiring Test Project/Assets/Scripts/GUI/GuiButtonBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuiButtonBehaviour : MonoBehaviour
+{
+    public Dictionary<int, GameObject> ButtonNames = new Dictionary<int, GameObject>();
+    GameObject buttonPanel;
+
+
+    void Start()
+    {
+        buttonPanel = transform.GetChild(0).gameObject;
+
+        NameButtons();
+    }
+
+    void NameButtons()
+    {
+        for (int i = 0; buttonPanel.transform.childCount > i; i++)
+        {
+            if (!ButtonNames.ContainsKey(i))
+            {
+                ButtonNames.Add(i, buttonPanel.transform.GetChild(i).gameObject); //Store Gameobjects
+                Debug.Log(i + ", " + buttonPanel.transform.GetChild(i).gameObject);
+            }
+        }
+        int j = 0;
+        foreach (KeyValuePair<int, GameObject> kvp in ButtonNames)
+        {
+            if (kvp.Value.name == buttonPanel.transform.GetChild(j).transform.name)
+            {
+                buttonPanel.transform.GetChild(j).GetChild(0).GetComponent<Text>().text = kvp.Value.name; //Change button text based on gameobject names
+            }
+            j++;
+        }
+    }
+  
+}
