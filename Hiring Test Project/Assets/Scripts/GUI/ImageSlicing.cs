10afb9d066410070191a3aa8e393956a9268296b
dauswectus <dausw93@gmail.com>
2019-09-05 03:08:01 +0200

    Puzzlemovement added, basic background added

--- Hiring Test Project/Assets/Scripts/GUI/ImageSlicing.cs
+++ Hiring Test Project/Assets/Scripts/GUI/ImageSlicing.cs
@@ -7,19 +7,23 @@ public class ImageSlicing : MonoBehaviour
     // Start is called before the first frame update
     public Texture2D tex;
     public GameObject puzzlePiecePrefab;
+    private int puzzleNumber;
     private Sprite mySprite;
     List<GameObject> PuzzlePieces = new List<GameObject>();
 
     void Start()
     {
+        Transform parentPanel = transform.GetChild(0);
         for(int j=0;j<4;j++)
         {
             for (int i = 0; i < 8; i++)
             {
                 mySprite = Sprite.Create(tex, new Rect(i * tex.width / 8, j * tex.height / 4, tex.width / 8, tex.height / 4), new Vector2(0.5f, 0.5f), 100.0f);
-                GameObject puzzlePieceObject = (GameObject)Instantiate(puzzlePiecePrefab, transform);
+                GameObject puzzlePieceObject = (GameObject)Instantiate(puzzlePiecePrefab,/* transform.GetChild(0),*/ transform);
                 puzzlePieceObject.transform.localScale = Vector3.one;
                 puzzlePieceObject.GetComponent<Image>().sprite = mySprite;
+                puzzlePieceObject.AddComponent<PuzzleMovement>();
+                puzzlePieceObject.AddComponent<Button>();
                 PuzzlePieces.Add(puzzlePieceObject);
             }
         }
@@ -27,10 +31,15 @@ public class ImageSlicing : MonoBehaviour
         for (int i = 0; i < puzzleCount-1; i++)
         {
             int rnd = Random.Range(0, PuzzlePieces.Count);
-            PuzzlePieces[rnd].transform.SetParent(transform.GetChild(0));
+            PuzzlePieces[rnd].transform.SetParent(parentPanel);
             PuzzlePieces.RemoveAt(rnd);
             Debug.Log(PuzzlePieces.Count);
         }
+        GameObject BlankPiece = (GameObject)Instantiate(puzzlePiecePrefab, parentPanel);
+        BlankPiece.transform.localScale = Vector3.one;
+        BlankPiece.name = "Blank";
+
+
     }
 
     // Update is called once per frame
---
+++ Hiring Test Project/Assets/Scripts/Mechanics/PuzzleMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzleMovement : MonoBehaviour
+{
+    private int puzzleNumber;
+    private Transform parentPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        parentPanel = transform.parent;
+        GetComponent<Button>().onClick.AddListener(MovePuzzle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MovePuzzle()
+    {
+        puzzleNumber = transform.GetSiblingIndex();
+
+        if (puzzleNumber > 0 && parentPanel.GetChild(puzzleNumber - 1).name == "Blank") //Before
+        {
+              transform.SetSiblingIndex(puzzleNumber - 1);
+        }
+
+        else if (puzzleNumber + 1 < parentPanel.childCount && parentPanel.GetChild(puzzleNumber + 1).name == "Blank") //After
+        {
+            transform.SetSiblingIndex(puzzleNumber + 1);
+        }
+
+        else if (puzzleNumber + 8 < parentPanel.childCount && parentPanel.GetChild(puzzleNumber + 8).name == "Blank" ) //Below
+        {
+            transform.SetSiblingIndex(puzzleNumber + 8);
+            parentPanel.GetChild(puzzleNumber + 7).SetSiblingIndex(puzzleNumber);
+        }
+        else if (puzzleNumber - 8 < parentPanel.childCount && parentPanel.GetChild(puzzleNumber - 8).name == "Blank") //Above
+        {
+            transform.SetSiblingIndex(puzzleNumber - 7);
+            parentPanel.GetChild(puzzleNumber - 8).SetSiblingIndex(puzzleNumber);
+        }
+        else
+        {
+            Debug.Log("Index Out of Bounds");
+        }
+       
+    }
+}
